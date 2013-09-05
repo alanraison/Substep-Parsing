8b5d007f7ec8670ae997f77a32413c2b8cfdad5b
Ricky Barefield <rbarefield@technophobia.com>
2013-09-05 17:11:07 +0100

    Problem parser working

--- src/main/scala/com/technophobia/subteps/ProblemParser.scala
+++ src/main/scala/com/technophobia/subteps/ProblemParser.scala
@@ -1,23 +1,22 @@
 package com.technophobia.subteps
 
-import scala.util.parsing.combinator.{PackratParsers, RegexParsers}
-import scala.util.parsing.combinator.PackratParsers.PackratParser
+import scala.util.parsing.combinator.RegexParsers
 
-class ProblemParser extends RegexParsers with PackratParsers {
+class ProblemParser extends RegexParsers {
 
   override val skipWhitespace = false
   override val whiteSpace                    = """[ \t]+""".r
 
-  def eol: Parser[Any]                       = """(\r?\n)+""".r
+  def eol: Parser[Any]                       = """\r?\n""".r
 
-  def sections : Parser[Any] = repsep(section, eol)
+  def sections : Parser[Any] = rep(section) ~ rep(eol)
 
-  def section : Parser[Any] = specificLine ~ lines
+  def section : Parser[Any] = specificLine ~ rep(line)
 
-  def lines : Parser[Any] = repsep(line, eol)
+  def specificLine : Parser[Any] = "Specific:" <~ line
 
-  def specificLine : Parser[Any] = "Specific:" ~> line
+  def line : Parser[Any] = """[^:\n\r]+""".r ~ rep1(eol)
 
-  def line : Parser[Any] = """[^:]+""".r
+  //  def specificLine : Parser[Any] = """Specific:[^:\n\r]+""".r ~ eol
 
 }
